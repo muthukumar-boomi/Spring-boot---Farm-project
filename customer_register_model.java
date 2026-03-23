28374ec9a7e38b4b52cd09677ff684bafe525fe0
muthukumar-boomi <muthukumarboomi1@gmail.com>
2026-03-23 19:07:46 +0530

    Add files via upload

---
+++ customer_register_model.java
@@ -0,0 +1,78 @@
+package springboot.Pure.Goat.Farms;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer_register")
+public class customer_register_model {
+    
+    @Id
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "pass")
+    private String pass;
+    
+    @Column(name = "mobile")
+    private String mobile;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "address")
+    private String address;
+    
+    // Constructors
+    public customer_register_model() {}
+    
+    public customer_register_model(String name, String pass, String mobile, String email, String address) {
+        this.name = name;
+        this.pass = pass;
+        this.mobile = mobile;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
